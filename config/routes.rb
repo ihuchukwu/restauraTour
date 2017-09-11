390a9186a0538ae5d8e388538b3246134ffa0010
Benjamin Busilana <venaswb@gmail.com>
2017-09-11 11:48:42 -0400

    added menu items

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  get  '/signup',  to: 'users#index'
 
-resources :users
-resources :restaurants
-resources :menu_items
-resources :events
-resources :about
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :restaurants
+  resources :menu_items
+  resources :events
+  resources :about
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
