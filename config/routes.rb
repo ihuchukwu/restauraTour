bdf602d0c5f5f786990dd97219c385e50aa94506
ihuchukwu <ugwumbadarling@yahoo.com>
2017-09-10 22:31:56 -0400

    testing test/models/user_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  root "static_pages#home"
+  get  '/signup',  to: 'users#new'
 
-resources :users
-resources :restaurants
-resources :menu_items
-resources :events
-resources :about
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :restaurants
+  resources :menu_items
+  resources :events
+  resources :about
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
