916c0d0839080a9ef3c232b52e767c92836a4cc2
Benjamin Busilana <venaswb@gmail.com>
2017-09-12 11:01:56 -0400

    update the menuitem controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,10 +1,11 @@
 class MenuItemsController < ApplicationController
   def index
+    @menu = menu_items_url.all
   end
 
   def show
-    restaurant = Restaurant.find_by_id(params[:id])
-    @menu = restaurant.MenuItems
+    # restaurant = Restaurant.find_by_id(params[:id])
+    # @menu = restaurant.menu_items
   end
 
   def new
