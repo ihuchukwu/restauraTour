1a5dd95d85c9def977386ec87fc87e01b3958015
ihuchukwu <ugwumbadarling@yahoo.com>
2017-09-13 07:55:15 -0400

    integrate carrierwave into Rails app

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,8 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update]
   def index
     @restaurants = Restaurant.all
+    @restaurant = Restaurant.order('created_at DESC')
   end
 
   def show
@@ -8,8 +10,36 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(post_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @restaurant.update_attributes(post_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :location, :image)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,14 @@ class Restaurant < ApplicationRecord
   has_many :menu_items
   has_many :events
   mount_uploader :image, ImageUploader
+
+  before_save :update_image_attributes
+
+  private
+    def update_image_attributes
+      if image.present? && image_changed?
+        self.image_content_type = image.file.content_type
+        self.image_file_size    = image.file.size
+      end
+    end
 end
